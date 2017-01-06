463441949e0673689f737a20389d7d90ac23e25a
ZhuBoao <361255448@qq.com>
2017-01-05 22:52:37 -0800

    add single character represent

--- shakyamuni/CharacterRepresent/CharacterRepresent.h
+++ shakyamuni/CharacterRepresent/CharacterRepresent.h
@@ -8,10 +8,18 @@
 #include <shakyamuni/Types/types.h>
 #include <iostream>
 #include <vector>
+#include <map>
 
 namespace skmn {
     class CharacterRepresent {
+    protected:
+        std::map<int, char> characterSet;
+        Features intensities;
     public:
+        void setIntensities(const Features &intensities);
+        void setCharacterSet(const std::map<int, char> &characterSet);
+        const Features &getIntensities() const;
+        const std::map<int, char> &getCharacterSet() const;
         virtual std::vector<std::string> represent() = 0;
 
     };

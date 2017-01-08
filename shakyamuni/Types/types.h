3e7c31a1274199c94ae0f9e72f6ebc5a10244d9f
ZhuBoao <361255448@qq.com>
2017-01-07 21:44:48 -0800

    modified:   shakyamuni/Types/types.h

--- shakyamuni/Types/types.h
+++ shakyamuni/Types/types.h
@@ -7,12 +7,16 @@
 
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
+#define GRAY_LEVEL_8 8;
+#define GRAY_LEVEL_4 4;
 
 namespace skmn {
+
     typedef cv::Mat Image;
     typedef cv::Size ImgSize;
     typedef cv::Mat Features;
     typedef cv::VideoCapture Video;
+
 }
 
 #endif //SHAKYAMUNI_TYPES_H
